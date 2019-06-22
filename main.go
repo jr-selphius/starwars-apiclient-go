8be301ed5cb5ea9d8d24e562529514ab98e6bbea
jr-selphius <sanrafja@hotmail.com>
2019-06-22 12:53:10 +0200

    The number 1 planet of the database is fetched, unmarshalled and printed by console

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"encoding/json"
+	"fmt"
+)
+
+func main() {
+
+	var planet Planet
+
+	resp, _ := http.Get("https://swapi.co/api/planets/1")
+	body, _ := ioutil.ReadAll(resp.Body)
+	_ = json.Unmarshal(body, &planet)
+	fmt.Println(planet)
+}
+
+type Planet struct {
+	Name           string `json:"name"`
+	RotationPeriod string `json:"rotation_period"`
+	OrbitalPeriod  string `json:"orbital_period"`
+	Diameter       string `json:"diameter"`
+	Climate        string `json:"climate"`
+	Gravity        string `json:"gravity"`
+	SurfaceWater   string `json:"surface_water"`
+	Population     string `json:"population"`
+	Created        string `json:"created"`
+	Edited         string `json:"edited"`
+	URL            string `json:"url"`
+}
