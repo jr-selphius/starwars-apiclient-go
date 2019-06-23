c2fc03d623ecda5dd9d3c20c1cb94de7d496a6f9
jr-selphius <sanrafja@hotmail.com>
2019-06-23 09:34:35 +0200

    Now the cli is taking the id as a parameter, to know what planet must fetch and save to a disk file

--- internal/cli/planets.go
+++ internal/cli/planets.go
@@ -1,22 +1,28 @@
 package cli
 
-import(
-	"github.com/spf13/cobra"
+import (
 	"encoding/csv"
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"os"
+
+	"github.com/spf13/cobra"
 )
 
 type CobraFn func(cmd *cobra.Command, args []string)
 
+const idFlag string = "id"
+
 func InitPlanetsCmd() *cobra.Command {
-	planetsCmd := &cobra.Command {
-		Use: "planets",
+	planetsCmd := &cobra.Command{
+		Use:   "planets",
 		Short: "Save planet to csv file",
-		Run: runPlanetsFn(),
+		Run:   runPlanetsFn(),
 	}
+
+	planetsCmd.Flags().StringP(idFlag, "i", "", "id of the planet")
+
 	return planetsCmd
 }
 
@@ -26,9 +32,13 @@ const APIResource string = "planets/"
 func runPlanetsFn() CobraFn {
 	return func(cmd *cobra.Command, args []string) {
 
-		var planet Planet
+		IDResource, _ := cmd.Flags().GetString(idFlag)
 
-		IDResource := "1"
+		if IDResource == "" {
+			IDResource = "1"
+		}
+
+		var planet Planet
 
 		resp, _ := http.Get(APIEndpoint + APIResource + IDResource)
 		body, _ := ioutil.ReadAll(resp.Body)
