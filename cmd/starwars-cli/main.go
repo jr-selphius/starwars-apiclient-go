0d47b08a2663715dbe999e3fa12751417f804071
jr-selphius <sanrafja@hotmail.com>
2019-06-23 22:35:58 +0200

    csvRepository and remoteRepository dependencies are now injected properly

--- cmd/starwars-cli/main.go
+++ cmd/starwars-cli/main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
-	"github.com/spf13/cobra"
+	"github.com/jr-selphius/starwars-apiclient-go/internal"
 	"github.com/jr-selphius/starwars-apiclient-go/internal/cli"
+	"github.com/jr-selphius/starwars-apiclient-go/internal/storage/csv"
+	"github.com/jr-selphius/starwars-apiclient-go/internal/storage/remote"
+	"github.com/spf13/cobra"
 )
 
 func main() {
+
+	var remoteRepository internal.PlanetRepo
+	remoteRepository = remote.NewRemoteRepository()
+
+	var csvRepository internal.PlanetRepo
+	csvRepository = csv.NewCsvRepository()
+
 	rootCmd := &cobra.Command{Use: "starwars-cli"}
-	rootCmd.AddCommand(cli.InitPlanetsCmd())
+	rootCmd.AddCommand(cli.InitPlanetsCmd(remoteRepository, csvRepository))
 	rootCmd.Execute()
 }
